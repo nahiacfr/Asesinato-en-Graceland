2cc8b3d09d020ddd9af59bac54733398a0ca75ab
HectorFC67 <hector.f.c@opendeusto.es>
2024-01-07 22:48:55 +0100

    juego hecho lets go

--- Assets/@MyAssets/Scripts/EndGame.cs
+++ Assets/@MyAssets/Scripts/EndGame.cs
@@ -1,33 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EndGame : MonoBehaviour
 {
     public GameObject culpable = null;
     public GameObject winVideo = null;
     public GameObject loseVideo = null;
-    private static bool win = false; 
+    private static bool win = false;
+
+    private VideoPlayer winVideoPlayer;
+    private VideoPlayer loseVideoPlayer;
+    private AudioSource winAudioSource;
+    private AudioSource loseAudioSource;
 
     void Start()
     {
+        winVideoPlayer = winVideo.GetComponent<VideoPlayer>();
+        loseVideoPlayer = loseVideo.GetComponent<VideoPlayer>();
+        winAudioSource = winVideo.GetComponent<AudioSource>();
+        loseAudioSource = loseVideo.GetComponent<AudioSource>();
+
+        winVideoPlayer.Stop();
+        loseVideoPlayer.Stop();
+        winAudioSource.Stop();
+        loseAudioSource.Stop();
+
         if (win)
         {
             loseVideo.SetActive(false);
             winVideo.SetActive(true);
+            winVideoPlayer.Play();
+            winAudioSource.Play();
         }
         else
         {
             loseVideo.SetActive(true);
             winVideo.SetActive(false);
+            loseVideoPlayer.Play();
+            loseAudioSource.Play();
         }
     }
 
     public void acuse(GameObject acusado)
     {
-        if(acusado == culpable)
+        if (acusado == culpable)
         {
             win = true;
             SceneManager.LoadScene("Lose");
