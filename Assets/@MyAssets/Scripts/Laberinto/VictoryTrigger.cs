c7c8e240e787a218bfca1bb932f52602c5b3afba
nahiacfr <nahia.castaneda@opendeusto.es>
2024-05-31 18:34:01 +0200

    Cambio script victoria

--- Assets/@MyAssets/Scripts/Laberinto/VictoryTrigger.cs
+++ Assets/@MyAssets/Scripts/Laberinto/VictoryTrigger.cs
@@ -2,17 +2,35 @@ using UnityEngine;
 
 public class VictoryTrigger : MonoBehaviour
 {
-    public GameObject victoryCanvas;
+    [SerializeField] private GameObject trofeoPrefab;
+    [SerializeField] private ParticleSystem confetti1;
+    [SerializeField] private ParticleSystem confetti2;
+    [SerializeField] private AudioSource victorySound;
 
+    private Vector3 playerNewPosition = new Vector3(3.610956f, 6.89674f, 10.80618f);
+    private Vector3 trofeoPosition = new Vector3(3.610956f, 6.89674f, 5.4f);
 
     void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Player"))
         {
 
-            victoryCanvas.SetActive(true);
-        }
-    }
+            other.transform.position = playerNewPosition;
 
+            Instantiate(trofeoPrefab, trofeoPosition, Quaternion.identity);
+
+            confetti1.gameObject.SetActive(true);
+            confetti2.gameObject.SetActive(true);
+
+            confetti1.Play();
+            confetti2.Play();
+
+            if (victorySound != null)
+            {
+                victorySound.Play();
+            }
+        }
+
+    }
 }
+
