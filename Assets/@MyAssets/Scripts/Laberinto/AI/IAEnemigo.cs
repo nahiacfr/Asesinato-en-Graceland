9821125c28a08ddc1949fae2d660f0e3e006dad9
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2024-06-06 13:30:12 +0200

    Solucionado problemas con las animaciones

--- Assets/@MyAssets/Scripts/Laberinto/AI/IAEnemigo.cs
+++ Assets/@MyAssets/Scripts/Laberinto/AI/IAEnemigo.cs
@@ -1,3 +1,4 @@
+using GLTF.Schema;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,26 @@ using UnityEngine.AI;
 public class IAEnemigo : MonoBehaviour
 {
     NavMeshAgent agent;
-    Animator anim;
+    public Animator anim;
     State currentState;
 
+    Transform actualP;
+    public Transform p1;
+    public Transform p2;
     public Transform player;
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-        currentState = new Idle(gameObject, agent, anim, player);
+        //agent = GetComponent<NavMeshAgent>();
+        //anim = GetComponent<Animator>();
+        //currentState = new Idle(gameObject, agent, anim, player);
+        actualP = p1;
+        GetComponent<NavMeshAgent>().destination = actualP.transform.position;
+        anim.SetTrigger("Walk");
     }
 
     void Update()
     {
-        currentState = currentState.Process();
+        //currentState = currentState.Process();
     }
 }
